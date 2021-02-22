71c2a20d41fd113d624bfe5dd8de7e7f711edba9
minh-bao <mbnguyen@hotmail.fr>
2021-02-22 19:40:11 +0100

    add mass assignement

--- app/Models/Site/Websitebloc.php
+++ app/Models/Site/Websitebloc.php
@@ -10,4 +10,8 @@ class Websitebloc extends Model
     use HasFactory;
     protected $table = 'siteblocs';
 
+    protected $fillable = [
+        'sitepages_id', 'sitesliders_id', 'title', 'content', 'conten_two', 'image', 'title_img', 'alt_img', 'url_image', 'format', 'sort', 'created_at', 'updated_at'
+    ];
+
 }
