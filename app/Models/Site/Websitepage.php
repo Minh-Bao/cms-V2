cea03cd8d79150c2cc14658178921cf6e51ef86d
minh-bao <mbnguyen@hotmail.fr>
2021-02-22 09:13:35 +0100

    add fillable field

--- app/Models/Site/Websitepage.php
+++ app/Models/Site/Websitepage.php
@@ -9,5 +9,23 @@ class Websitepage extends Model
 {
     use HasFactory;
     protected $table = 'sitepages';
-
+    protected $fillable = [
+        '_token',
+        'last_review',
+        'paginate',
+        'status',
+        'name',
+        'title',
+        'title_article',
+        'thumbnail',
+        'image',
+        'alt_img',
+        'title_img',
+        'meta_title',
+        'meta_desc',
+        'author',
+        'content',
+        'count',
+        'slider_id'
+    ];
 }
