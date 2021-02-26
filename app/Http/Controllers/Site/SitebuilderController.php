e347a76bc0d9dedc26e2097e12d71773523fd22b
minh-bao <mbnguyen@hotmail.fr>
2021-02-26 19:48:24 +0100

    comment unused code

--- app/Http/Controllers/Site/SitebuilderController.php
+++ app/Http/Controllers/Site/SitebuilderController.php
@@ -8,8 +8,6 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\App;
 use App\Models\Site\Websitepage;
@@ -18,7 +16,6 @@ use App\Slim;
 use App\Models\Site\Slider;
 use App\Models\Site\Sliderimage;
 use Image;
-use Auth;
 use DB;
 use Session;
 
@@ -49,11 +46,11 @@ class SitebuilderController extends Controller
         $variable = $request->get("elem");
         $object = $request->get("object");   
 
-        if ($part=="config") {                        
+/*         if ($part=="config") {                        
             $config = Siteconfig::select('*')->whereVariable($variable)->first();
             $object="";
         }
-        
+ */        
         if ($part=="bloc") { 
             $config = Websitebloc::select('*')->where('id',$variable)->first();
             $config->content = $config->$object;
@@ -96,7 +93,7 @@ class SitebuilderController extends Controller
 
     public function update(Request $request,$id)
     {
-      if ($request->part=="config") {
+/*       if ($request->part=="config") {
         $lng = App::getLocale();
         $configlng = "content_".$lng;
         $config = Siteconfig::find($id);
@@ -141,7 +138,7 @@ class SitebuilderController extends Controller
         }
         $config->save();
     }
-
+ */
     if ($request->part=="bloc") {
         $bloc = Websitebloc::find($id);
         $page = Websitepage::find($bloc->sitepages_id);
@@ -296,7 +293,7 @@ class SitebuilderController extends Controller
         $page->save();       
     }
 
-    if ($request->part=="partenaire") {
+/*     if ($request->part=="partenaire") {
         $siteconfig = Siteconfig::select('*')->orderBy('id')->get();
         $bloc = Partenaire::find($id);
 
@@ -331,5 +328,7 @@ class SitebuilderController extends Controller
 
         return back()->withInput();       
     }
+ */
+
 }
 
