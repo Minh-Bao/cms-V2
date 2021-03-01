d64eb706810459815d9764bb97d91618d5778c1f
minh-bao <mbnguyen@hotmail.fr>
2021-03-01 09:42:36 +0100

    create a request rules validation class for Contact email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,28 +9,23 @@ use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    public function send(Request $request)
+    /**
+     * Retrieve and validate input data and send an email to contact
+     *
+     * @param ContactRequest $request
+     * @return view
+     */
+    public function send(ContactRequest $request)
     {
-    
-        // $this->validate($request, [
-        //     'name'         => 'required',
-        //     'email'        => 'required|min:2|max:150',
-        //     'subject'       => 'required',
-        //     'message'       => 'required',                
-        // ]);
-    
         $name = utf8_decode($request->name);
         $email = utf8_decode($request->email);
         $subject = utf8_decode($request->subject);
         $msg = utf8_decode($request->message);
 
-
-
         $data = compact('name', 'email', 'subject', 'msg');
 
         Mail::send('emails.contact', $data, function($message){
             $message->to('contact@naturelcoquin.com', 'Blog.naturelCoquin')->subject('Contact mail blog');   
-
         });
 
         return back();
