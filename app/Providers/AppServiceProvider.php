46f107b19a5d868dde86e902e1b37367902a4aaf
minh-bao <mbnguyen@hotmail.fr>
2021-03-01 09:45:45 +0100

    register bootstrap instead of tailwind for paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use App\Repositories\SliderRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\SliderImageRepository;
@@ -48,6 +49,7 @@ class AppServiceProvider extends ServiceProvider
             SliderImageRepository::class,
         );
 
+        Paginator::useBootstrap();
 
     }
 }
