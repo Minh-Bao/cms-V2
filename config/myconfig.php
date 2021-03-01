650ef81d001ce96173568d35c185fbcfce1731cc
minh-bao <mbnguyen@hotmail.fr>
2021-03-01 16:11:47 +0100

    create custom config file for env variables

---
+++ config/myconfig.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+   
+
+    'site_owner' => env('SITE_OWNER', 'null'),
+    'site_developper' =>env('SITE_DEVELOPPER', 'null'),
+    'site_developper_url' => env('SITE_DEVELOPPER_URL', 'null'),
+    'name_app' =>env('APP_NAME', 'null'),
+    'site_theme' =>env('SITE_THEME', 'null'),
+    'FB_owner_url' => env('FB_OWNER_URL', 'http://facebook.com'),
+    'utube_owner_url' =>env('UTUBE_OWNER_URL', 'http://youtube.com'),
+    'pinterest_owner_url' => env('PINTEREST_OWNER_URL', 'http://pinterrest.com'),
+    'insta_owner_url' => env('INSTA_OWNER_URL', 'http://instagram.com')
+];
