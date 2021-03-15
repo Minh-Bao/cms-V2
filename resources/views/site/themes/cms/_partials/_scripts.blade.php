f31287791512e971939d05d0795c74516228b44c
minh-bao <mbnguyen@hotmail.fr>
2021-03-15 11:22:56 +0100

    delete jquery and add alpine.js

--- resources/views/site/themes/cms/_partials/_scripts.blade.php
+++ resources/views/site/themes/cms/_partials/_scripts.blade.php
@@ -1,12 +1,13 @@
-<script src="{{url('/')}}/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<script src="{{url('/')}}/css/owlcarousel/owl.carousel.js"></script>
-{{-- 
+<script src="{{url('/css/owlcarousel/owl.carousel.js')}}"></script>
+<script src="{{url('/js/app.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+
 <script>
 
 
-$(document).ready(function() {
+/* $(document).ready(function() {
     $('#form-contact').submit(function() {
         $.post(
             '{{ route('site.send.form') }}',
@@ -32,7 +33,7 @@ $(document).ready(function() {
         return false;
     });
 });
-
+ */
 
 
 
@@ -145,4 +146,3 @@ $(function() {
 
 </script>
 
- --}}
