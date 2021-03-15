181a8a56be49e864b21de8c2b97c6fa736cb211e
minh-bao <mbnguyen@hotmail.fr>
2021-03-15 11:20:13 +0100

    npm build dev

--- public/js/instafeed-main.js
+++ public/js/instafeed-main.js
@@ -1,1 +1,16 @@
-new Instafeed({accessToken:"IGQVJXNkVIMkdDcHNLaE5wSk92WUFtTTlXaEFvd0lrUFROVWRuXy10MGJsRktzelRJSVBHRVdpQ1dxcDd0czFqMnZA1dTE5MmxMcVFYeWVYRHc4X0lSQTI0YnJzc1VEUm5sVGcwSnJNMzBIVE1zYmQzOAZDZD",limit:12,template:'<a class="col-md-3" target="_blank href="{{link}}"><img class="img-thumbnail shadow  bg-white rounded img_insta" title="{{caption}}" src="{{image}}" /></a>',error:function(){document.getElementById("insta_error_msg").style.display="block"}}).run();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************!*\
+  !*** ./resources/js/instafeed-main.js ***!
+  \****************************************/
+var feed = new Instafeed({
+  accessToken: 'IGQVJXNkVIMkdDcHNLaE5wSk92WUFtTTlXaEFvd0lrUFROVWRuXy10MGJsRktzelRJSVBHRVdpQ1dxcDd0czFqMnZA1dTE5MmxMcVFYeWVYRHc4X0lSQTI0YnJzc1VEUm5sVGcwSnJNMzBIVE1zYmQzOAZDZD',
+  limit: 12,
+  template: '<a class="col-md-3" target="_blank href="{{link}}"><img class="img-thumbnail shadow  bg-white rounded img_insta" title="{{caption}}" src="{{image}}" /></a>',
+  error: function error() {
+    document.getElementById('insta_error_msg').style.display = "block";
+  }
+});
+feed.run();
+/******/ })()
+;
