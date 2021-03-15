fa66612027d3b092689cf475d8c6d00109d4e4e5
minh-bao <mbnguyen@hotmail.fr>
2021-03-15 20:52:17 +0100

    fix footer style and add modal with tailwind and alpineJS

--- resources/views/site/themes/cms/_partials/_footer.blade.php
+++ resources/views/site/themes/cms/_partials/_footer.blade.php
@@ -1,60 +1,89 @@
 <!-- FOOTER -->
 
-<footer class="footer-container" style="height: 300px;">
-    <div class="flex flex-wrap ">
+<footer class="footer-container" style="height: 300px;" x-data="{ 'showModal': false }"
+    @keydown.escape="showModal = false" x-cloak>
+    <div class="flex flex-wrap bg-black">
         <div class="md:w-1/4 pr-4 pl-4 ml-5 mb-5">
             <div>
                 <img src="http://localhost:8000/images/logo-white.png" alt="" width="50%" class="mt-5"><br>
             </div>
             <div class="m-2 ml-2">
-                <a href="{{ config('myconfig.FB_owner_url') }}" target="_blank"  title="facebook_{{config('myconfig.site_owner')}}" >
-                    <img src="{{url('/images/socialMedia_icon/facebook_blanc.png')}}" alt="icon_facebook" width="10%">
+                <a href="{{ config('myconfig.FB_owner_url') }}" target="_blank"
+                    title="facebook_{{ config('myconfig.site_owner') }}">
+                    <img src="{{ url('/images/socialMedia_icon/facebook_blanc.png') }}" alt="icon_facebook" width="10%">
                 </a>
-                <a href="{{ config('myconfig.utube_owner_url') }}" target="_blank" title="youtube_{{config('myconfig.site_owner')}}" >
-                    <img src="{{url('/images/socialMedia_icon/youtube_blanc.png')}}" alt="icon_youtube" width="10%" >
+                <a href="{{ config('myconfig.utube_owner_url') }}" target="_blank"
+                    title="youtube_{{ config('myconfig.site_owner') }}">
+                    <img src="{{ url('/images/socialMedia_icon/youtube_blanc.png') }}" alt="icon_youtube" width="10%">
                 </a>
-                <a href="{{ config('myconfig.pinterest_owner_url') }}" target="_blank" title="pinterest_{{config('myconfig.site_owner')}}">
-                    <img src="{{url('/images/socialMedia_icon/pinterest_blanc.png')}}" alt="icon_pinterest" width="10%" >
+                <a href="{{ config('myconfig.pinterest_owner_url') }}" target="_blank"
+                    title="pinterest_{{ config('myconfig.site_owner') }}">
+                    <img src="{{ url('/images/socialMedia_icon/pinterest_blanc.png') }}" alt="icon_pinterest"
+                        width="10%">
                 </a>
-                <a href="{{ config('myconfig.insta_owner_url') }}" target="_blank" title="instagram_{{config('myconfig.site_owner')}}">
-                    <img src="{{url('/images/socialMedia_icon/instagram_blanc.png')}}" alt="icon_instagram" width="10%" >
+                <a href="{{ config('myconfig.insta_owner_url') }}" target="_blank"
+                    title="instagram_{{ config('myconfig.site_owner') }}">
+                    <img src="{{ url('/images/socialMedia_icon/instagram_blanc.png') }}" alt="icon_instagram"
+                        width="10%">
                 </a>
             </div>
         </div>
         <div class="md:w-1/4 pr-4 pl-4" style="margin-top:10%">
-            <a href="{{config('myconfig.site_developper_url')}}">© {{config('myconfig.site_developper')}} {{date('Y')}} -</a>  
-            <a href="javascript:void(0);" data-toggle="modal" data-target="#mentions">Mentions légales</a>
+            <a href="{{ config('myconfig.site_developper_url') }}">© {{ config('myconfig.site_developper') }}
+                {{ date('Y') }} -</a>
+            <button type="button"
+                class="bg-transparent  hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full"
+                @click="showModal = true">Mentions Légales</button>
         </div>
 
         <div class="copyright-text text-left md:w-1/4 pr-4 pl-4" style="margin-top:3%; margin-left:5%;">
-            <h3 class="uppertext " style="font-size: 2.7rem !important;font-weight: bolder !important;border-bottom: 3px solid #ce4963;">
+            <h3 class="uppertext "
+                style="font-size: 2.7rem !important;font-weight: bolder !important;border-bottom: 3px solid #ce4963;">
                 Newsletter
             </h3>
             <p>Saisissez votre adresse pour vous abonner a notre newsletter...</p>
             <div class="relative flex items-stretch w-full mb-3">
-                <input type="text" class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" placeholder="adresse mail" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <input type="text"
+                    class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded"
+                    placeholder="adresse mail" aria-label="Recipient's username" aria-describedby="button-addon2">
                 <div class="input-group-append">
-                    <button class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline text-gray-600 border-gray-600 hover:bg-gray-600 hover:text-white bg-white hover:bg-gray-700" type="button" id="button-addon2" style="height:85%; top:15%;background-color: #ce4963;color:white;">Envoyer</button>
+                    <button
+                        class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline text-gray-600 border-gray-600 hover:bg-gray-600 hover:text-white bg-white"
+                        type="button" id="button-addon2"
+                        style="height:85%; top:15%;background-color: #ce4963;color:white;">Envoyer</button>
                 </div>
-            </div>         
             </div>
-        </div>
-</footer>
-
-  <!-- Modal -->
-
-<div id="mentions" class="modal opacity-0" role="dialog">
-    <div class="modal-dialog">
-
-      <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2 class="modal-title">{!! App\Models\Site\Websitepage::where('id',2)->first()->title !!}</h2>
-            </div>
-            <div class="modal-body">
-                {!! App\Models\Site\Websitepage::where('id',2)->first()->content !!}
-            </div>
-            <div class="modal-footer"></div>
         </div>
     </div>
-</div>
+
+    <!-- Modal -->
+
+    <div class="w-full bg-transparent" style="top:360%;" x-show="showModal"
+        :class="{ 'absolute z-10 flex items-center justify-center': showModal }">
+        <!--Dialog-->
+        <div class="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg py-4 text-left px-6" x-show="showModal"
+            @click.away="showModal = false" x-transition:enter="ease-out duration-300"
+            x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="ease-in duration-300" x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-90">
+
+            <!--Title-->
+            <div class="flex justify-between items-center pb-3">
+                <h2 class="text-2xl font-bold">{!! App\Models\Site\Websitepage::where('id', 2)->first()->title !!}</h2>
+                <div class="cursor-pointer z-50" @click="showModal = false">
+                    <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                        viewBox="0 0 18 18">
+                        <path
+                            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                        </path>
+                    </svg>
+                </div>
+            </div>
+
+            <!-- content -->
+            {!! App\Models\Site\Websitepage::where('id', 2)->first()->content !!}
+        </div>
+        <!--/Dialog -->
+    </div>
+    <!-- /Overlay -->
+</footer>
