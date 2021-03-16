ed20613efaeeb112c8e9a02b5f9ba6bb7676bb67
minh-bao <mbnguyen@hotmail.fr>
2021-03-16 23:43:49 +0100

    fix indentation

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -55,6 +55,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+
         if($exception instanceof NotFoundHttpException){
             return response()->view('errors/404', ['invalid_url'=>true], 404);
         }
