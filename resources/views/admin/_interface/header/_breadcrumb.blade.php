eaa23704a5a39d4fd2f9ec03644eeb1cbd5752a8
minh-bao <mbnguyen@hotmail.fr>
2021-03-22 22:09:14 +0100

    delete useless code and class

--- resources/views/admin/_interface/header/_breadcrumb.blade.php
+++ resources/views/admin/_interface/header/_breadcrumb.blade.php
@@ -1,6 +1,3 @@
-<!-- ============ Header file ================ -->
-
-
  <!-- Modal Dialogs ====================================================================================================================== -->
 
     <!-- Large Size -->
@@ -28,7 +25,7 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline font-normal text-blue-700 bg-transparent waves-effect" data-dismiss="modal">Fermer</button>
+                    <button type="button" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline text-blue-700 bg-transparent waves-effect" data-dismiss="modal">Fermer</button>
                 </div>
             </div>
         </div>
@@ -38,69 +35,45 @@
 
 
 
-  <!-- Top Bar -->
+  <!-- HEADER -->
                       <!-- Page title & actions -->
                       <header>
-                        <div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+                        <div class=" px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
                             <div id="breadcontainer">
-                                <ol class="flex flex-wrap list-reset pt-3 pb-3 py-4 px-4 mb-4 bg-gray-200 rounded">
-                                    {{-- <li><i class="material-icons">dashboard</i> <a href="{{url('/admin')}}/admin"> Accueil</a></li>
-                                    <li class="active"><i class="material-icons">description</i>  <a href="{{ route('websitepage.index') }}">Pages</a></li> --}}
-                                    
+                                <ol class="flex flex-wrap list-reset pt-3 pb-3 py-4 px-4 bg-transparent rounded ">
                                     @foreach ($bread as $item)
                                     <li>
                                         {!! $item['icon'] !!}
                                     </li>
-                                    <a href="{{ $item['url'] }}">
-                                        <li>
+                                    &ensp;
+                                    <li>
+                                        <a href="{{ $item['url'] }}" class="text-gray-600">
                                             {{ $item['name']  }}
-                                        </li>
-                                    </a>
+                                        </a>
+                                    </li>
+                                    &ensp;
                                     @endforeach
                                 
                                 </ol>
                             </div>
-                            <div class="flex-1 min-w-0">
-                                {{-- <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
+                            {{-- <div class="flex-1 min-w-0">
+                                <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
                                     Home
-                                </h1> --}}
-                            </div>
+                                </h1>
+                            </div> --}}
                             <div class="mt-4 flex sm:mt-0 sm:ml-4">
                                 <button type="button"
                                     class="order-1 ml-3 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-0 sm:ml-0">
                                     Share
                                 </button>
                                 <button type="button"
-                                    class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
+                                    class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-300 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
                                     Create
                                 </button>
                             </div>
                         </div>
                     </header>
-
-
-  {{-- <nav class="relative flex flex-wrap items-center content-between py-3 px-4 top_navbar_admin">
-    <div class="container mx-auto sm:px-4 max-w-full">
-        
-
-        <div class="site-logo text-center">
-            <a  href="{{route('site.homepage')}}"><img class="pb-5" src="{{url('/')}}/images/logo-black.png" rel="" alt="logo-naturelcoquin" width="40%"/></a>
-        </div>
-
-        <div class="navbar-header pull-left">
-            <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
-            <a href="javascript:void(0);" class="bars"></a>
-        </div>
- 
-
-        <div class="hidden flex-grow items-center" id="navbar-collapse">
-            <ul class="flex flex-wrap list-none pl-0 mb-0 list-reset navbar-right">
-            </ul>
-        </div>
-    </div>
-</nav> --}}
-
-<!-- #Top Bar -->
+<!-- #HEADER -->
 
 
 
