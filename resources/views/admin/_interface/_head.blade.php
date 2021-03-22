d20535ba940f0267c543fed84e555a52b089df05
minh-bao <mbnguyen@hotmail.fr>
2021-03-22 19:32:57 +0100

    comment almost all style excepts jquery for replacement

--- resources/views/admin/_interface/_head.blade.php
+++ resources/views/admin/_interface/_head.blade.php
@@ -17,27 +17,27 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-	{{ Html::style('plugins/bootstrap/css/bootstrap.css')}}
-    {{ Html::style('plugins/bootstrap/css/bootstrap-xlgrid.css')}}
+	{{-- {{ Html::style('plugins/bootstrap/css/bootstrap.css')}} --}}
+    {{-- {{ Html::style('plugins/bootstrap/css/bootstrap-xlgrid.css')}} --}}
 
     <!-- Waves Effect Css -->
-	{{ Html::style('plugins/node-waves/0.7.6/waves.min.css')}}
+	{{-- {{ Html::style('plugins/node-waves/0.7.6/waves.min.css')}} --}}
 
     <!-- Animation Css -->
-    {{ Html::style('plugins/animate-css/animate.css')}}
+    {{-- {{ Html::style('plugins/animate-css/animate.css')}} --}}
     {{-- <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> --}}
 
     <!-- Select2 -->
-    {!! Html::style('plugins/select2/select2.css') !!}
+    {{-- {!! Html::style('plugins/select2/select2.css') !!} --}}
 
     <!-- Custom Css (Add css to resource/sass/admin.scss and run "npm run dev" in dev mode then build in production)-->
     <link href="{{ asset('css/admin/admin.css') }}" rel="stylesheet">
 
     <!-- Ribbon -->
-    {{ Html::style('css/admin/ribbon.css')}}
+    {{-- {{ Html::style('css/admin/ribbon.css')}} --}}
 
     <!-- User Card -->
-    {{ Html::style('css/admin/user-card.css')}}
+    {{-- {{ Html::style('css/admin/user-card.css')}} --}}
     
     <link rel="shortcut icon" href="#">
     
@@ -45,6 +45,13 @@
     {{ Html::script('plugins/jquery.toaster/jquery.toaster.js')}}
     {{ Html::style('plugins/jquery.toaster/toastr.css')}}
     {{ Html::script('plugins/jquery.toaster/toastr.min.js')}}
+
+
+    <!-- Livewire -->
+    @livewireStyles
+
+
+    <!--  -->
 
     
     
