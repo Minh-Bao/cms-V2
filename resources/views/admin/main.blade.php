cbb571dbd3ba336110b7386b620907152d1ad926
minh-bao <mbnguyen@hotmail.fr>
2021-03-22 19:32:29 +0100

    add tailwind template and animate with alpine

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -7,31 +7,78 @@
 
     </head>
 
-
     <body id="body" class="">
-        <div class="se-pre-con"></div>
-        @include('admin._interface._nav')
-        @include('admin._interface._timeout')
-        @include('admin._interface._sidebar')
+    
+        <div class="h-screen flex overflow-hidden bg-white"
+        x-data=" { open: true }">
 
-        <!-- Main content --> 
-        <section class="content">
-            <div class="flex flex-wrap ">
-                <div class="md:w-full pr-4 pl-4">
+            @include('admin._interface._sidebar')
+
+            <!-- Main column -->
+            <div class="flex flex-col w-0 flex-1 overflow-hidden">
+
+                <!-- Search header Mobile screen -->
+                <div
+                    class="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:hidden">
+                    <!-- Sidebar toggle, controls the 'sidebarOpen' sidebar state. -->
+                    <button
+                        @click="open = !open"
+                        class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500 lg:hidden">
+                        <span class="sr-only">Open sidebar</span>
+                        <!-- Heroicon name: outline/menu-alt-1 -->
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </button>
+                    <div class="flex-1 flex justify-between px-4 sm:px-6 lg:px-8">
+                        <div class="flex-1 flex">
+                            <form class="w-full flex md:ml-0" action="#" method="GET">
+                                <label for="search_field" class="sr-only">Search</label>
+                                <div class="relative w-full text-gray-400 focus-within:text-gray-600">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                                        <!-- Heroicon name: solid/search -->
+                                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                            fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd"
+                                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input id="search_field" name="search_field"
+                                        class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent focus:placeholder-gray-400 sm:text-sm"
+                                        placeholder="Search" type="search">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabindex="0">
+
+                    <!-- Main content-->
                     @include('admin._interface._message')
                     @yield('content')
-                </div>
+
+
+                </main>
             </div>
-        </section>
-
-        <!-- Main Scripts -->
-
+        </div>
+    
         @include('admin._interface._scripts')
-
+    
         <!-- Additionnal Scripts -->
-
+    
         @yield('scripts')
-
+    
     </body>
 </html>
+
+
+
+
+
+
+
+
 
