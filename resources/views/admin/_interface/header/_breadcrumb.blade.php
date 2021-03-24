d9ad7958c1d15b14059333441a7603ee2ad7176a
minh-bao <mbnguyen@hotmail.fr>
2021-03-24 21:36:43 +0100

    add url for breadcrumb link

--- resources/views/admin/_interface/header/_breadcrumb.blade.php
+++ resources/views/admin/_interface/header/_breadcrumb.blade.php
@@ -61,7 +61,7 @@
                                             <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
                                         </svg>
                                         <i class="material-icons material-icons text-gray-400 px-2 rounded-md">{{$item['icon'] }}</i>                                        
-                                        <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{{$item['name']}}</a>
+                                        <a href="{{url('')}}{{$item['url']}}" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{{$item['name']}}</a>
                                     </div>
                                 </li>
                                 @endforeach
