c6e66327737a4e8c61468e47d21556dd52ed3ae6
minh-bao <mbnguyen@hotmail.fr>
2021-04-07 15:40:40 +0100

    add breadcrumb

--- resources/views/admin/site/slider/create.blade.php
+++ resources/views/admin/site/slider/create.blade.php
@@ -8,15 +8,29 @@
 
 @section('content')
 
+@include('admin._interface.header._breadcrumb', [
+    'bread' => [
+        [
+            'icon' => 'dashboard',
+            'url'  => '{{url("/admin")}}',
+            'name' => 'Accueil'
+        ],
+        [
+            'icon' => 'public',
+            'url'  => '{{url("/")}}',
+            'name' => 'Site'
+        ],
+        [
+            'icon' => 'photo_library',
+            'url'  => '{{url("/")}}',
+            'name' => 'Gestion des slides'
+        ]
+    ]
+])
+
 {!! Form::open(['route' => 'slider.store' ,'data-parsley-validate' =>'','files'=>true  ]) !!}
     <div class="container mx-auto sm:px-4 max-w-full">
-      <div id="breadcontainer">
-          <ol class="flex flex-wrap list-reset pt-3 pb-3 py-4 px-4 mb-4 bg-gray-200 rounded">
-              <li><i class="material-icons">dashboard</i> <a href="{{url('')}}/admin"> Accueil</a></li>
-              <li><i class="material-icons">public</i> Site</li>
-              <li><i class="material-icons">photo_library</i> <a href="{{route('slider.index')}}">Sliders</a></li>
-          </ol>
-      </div>
+      
         <div class="flex flex-wrap  clearfix">
             <div class="sm:w-full pr-4 pl-4 md:w-full lg:w-3/4">
                 <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
