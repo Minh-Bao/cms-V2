8ea2ef499ee1814fe521956fe13427a8baf133d7
minh-bao <mbnguyen@hotmail.fr>
2021-04-15 19:59:46 +0200

    fix syntax

--- resources/views/site/themes/cms/_partials/_scripts.blade.php
+++ resources/views/site/themes/cms/_partials/_scripts.blade.php
@@ -1,6 +1,6 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <script src="{{url('/css/owlcarousel/owl.carousel.js')}}"></script>
 <!-- Livewire -->
@@ -10,7 +10,8 @@
 <script src="{{url('/js/app.js')}}" defer></script>
 
 
-{{-- <script>
+{{-- 
+    <script>
 $(document).ready(function() {
     $('#form-contact').submit(function() {
         $.post(
@@ -38,6 +39,7 @@ $(document).ready(function() {
     });
 });
 </script>
+
  --}}
 
 
