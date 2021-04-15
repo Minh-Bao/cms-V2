ae5c37df8c409c607dcd54e21a7aba0161727ffe
minh-bao <mbnguyen@hotmail.fr>
2021-04-15 19:58:19 +0200

    fix indentation

--- resources/views/admin/site/websitepage/index.blade.php
+++ resources/views/admin/site/websitepage/index.blade.php
@@ -111,7 +111,6 @@
                                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                                                     role="menuitem">View
                                                 </a>
-                                                
                                             </div>
                                             <div class="py-1" role="none">
                                                 <a href="#"
