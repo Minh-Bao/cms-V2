8cfa48431f019bf5956273984ba645db0af4a2e4
minh-bao <mbnguyen@hotmail.fr>
2021-04-16 19:07:27 +0200

    create separate instafeed js file for small instagram feed

---
+++ resources/js/instafeed-small.js
@@ -0,0 +1,24 @@
+/**
+ * Instafeed script to call instagramm account 12 last images.
+ */
+ var feed = new Instafeed({
+    accessToken: process.env.MIX_INSTA_TOKEN,
+    limit: 6,
+    after: function() {
+        var container = document.getElementById('instafeed');
+        for (var i = 0; i < container.children.length; i++) {
+            var parent = container.children;
+            for (var j = 0; j < parent.length; j++) {
+                var child_img = parent[j].children;
+                child_img[0].setAttribute("class", "shadow  bg-white rounded-lg ");
+                parent[j].setAttribute('target', '_blank');
+            }
+        }
+    },
+    error: function() {
+        document.getElementById('insta_error_msg').style.display = "block";
+    }
+});
+
+//run the feed
+feed.run();
