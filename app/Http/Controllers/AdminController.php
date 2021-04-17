063506abe59e70fcf0c0520395bdbc804bc803c1
minh-bao <mbnguyen@hotmail.fr>
2021-04-17 23:30:05 +0200

    fix syntaxe

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:');
+        $this->middleware('auth');
     }
 
     /**
@@ -23,6 +23,7 @@ class AdminController extends Controller
      */
     public function index()
     {
+
         return view('admin.dashboard');
     }
 }
