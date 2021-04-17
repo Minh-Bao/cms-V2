00f893a5063c3d17d80abda379de774d538aa764
minh-bao <mbnguyen@hotmail.fr>
2021-04-17 23:35:37 +0200

    fix whitespace and indentation

--- resources/views/admin/_interface/_control.blade.php
+++ resources/views/admin/_interface/_control.blade.php
@@ -185,8 +185,6 @@
 
 <!-- /.control-sidebar -->
 
-<!-- Add the sidebar's background. This div must be placed
-
-       immediately after the control sidebar -->
+<!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
 
 <div class="control-sidebar-bg"></div>
--- resources/views/admin/_interface/_footer.blade.php
+++ resources/views/admin/_interface/_footer.blade.php
@@ -1,7 +1,10 @@
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 0.2.8
-    </div>
-    <strong>Copyright &copy; 2018 <a href="#">My Home Connexion</a>.</strong> All rights
-    reserved.
-  </footer>
+<footer class="main-footer">
+
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 0.2.8
+    </div>
+
+    <strong>Copyright &copy; 2018 <a href="#">My Home Connexion</a>.</strong> All rights reserved.
+
+</footer>
+
