ecf087009b2628a979214a59bdc86b93a6474859
minh-bao <mbnguyen@hotmail.fr>
2021-04-21 09:41:46 +0200

    fix edit page responsive view

--- resources/views/admin/site/slider/edit.blade.php
+++ resources/views/admin/site/slider/edit.blade.php
@@ -34,74 +34,70 @@
     ]
 ])
 
-    <div class="container mx-auto  max-w-full">
-        <div class="mx-8 py-3 px-6 mb-0 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-100  {{-- bg-gray-200 --}} border-b-1 border-gray-300 text-gray-900 ">
-            <div  class="">
-                <h2>Sliders</h2>
-            </div>
-        </div>
+    <div class="container mx-auto sm:px-4 max-w-full">
+        <div class="flex flex-wrap  clearfix">
+            <div class="md:w-full pr-4 pl-4 lg:w-3/4">
 
-        <div class="flex flex-wrap  mt-6">
-            <div class="w-6/8 max-w-6/8 mx-8">
                 <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
-                    <div class="">
+                    <div class="flex flex-wrap py-3 px-6 mb-0 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-100  {{-- bg-gray-200 --}} border-b-1 border-gray-300 text-gray-900 ">
+                        <div  class=" w-1/2 text-gray-500">
+                            <h2>Sliders</h2>
+                        </div>
+                    </div>                   
+
+                    <div class="body">
                         <div class="flex flex-wrap ">
                             <div class="md:w-full pr-4 pl-4">
                                 <div class="flex flex-wrap  mb-4">
-                                    <div class="w-full" id="sortable">
-                                        <ul class="sortable">
-                                            @foreach($pictures as $picture)
-                                                <li id="item-{{$picture->id}}">
-                                                    <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 droppable" id="{{$picture->id}}" name="noname">
-                                                        <div class="justify-between" >
-                                                            <img src="{{ asset($picture->picture)}}" class="">
-                                                            <a href="{{ route('sliderimage.edit',$picture->id) }}" class="mr-8 inline-block align-middle text-center text-green-300 select-none font-normal whitespace-no-wrap  py-1 px-3 leading-normal no-underline" id="{{$picture->id}}">
-                                                                <i class="material-icons">mode_edit</i>
-                                                            </a>
-                                                            <a href="{{ route('sliderimage.delete',$picture->id) }}" class="ml-8 inline-block align-middle text-center text-red-500 select-none font-normal whitespace-no-wrap  py-1 px-3 leading-normal no-underline ">
-                                                                <i class="material-icons">delete</i>
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                    <div class="saved">
-
-                                    </div>
-                                    <button 
-                                        class="save bg-purple-500 text-gray-50 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                        Sauvegarder
-                                    </button>
+                                    <div class="md:w-full pr-4 pl-4" id="sortable">
+                                    <ul class="sortable">
+                                        @foreach($pictures as $picture)
+                                            <li id="item-{{$picture->id}}">
+                                                <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 droppable" id="{{$picture->id}}" name="noname">
+                                                    <div class="body">
+                                                        <img src="{{ asset($picture->picture)}}" class="img-responsive" style="margin-bottom:10px;width:100%;height:auto;">
+                                                        <a href="{{ route('sliderimage.edit',$picture->id) }}" class="mr-8 inline-block align-middle text-center text-green-300 select-none font-normal whitespace-no-wrap  py-1 px-3 leading-normal no-underline" id="{{$picture->id}}">
+                                                            <i class="material-icons">mode_edit</i>
+                                                        </a>
+                                                        <a href="{{ route('sliderimage.delete',$picture->id) }}" class="ml-8 inline-block align-middle text-center text-red-500 select-none font-normal whitespace-no-wrap  py-1 px-3 leading-normal no-underline ">
+                                                            <i class="material-icons">delete</i>
+                                                        </a>                                                    </div>
+                                                </div>
+                                            </li>
+                                        @endforeach
+                                    </ul>
                                 </div>
-                            </div>
+                                <div class="saved">
 
-                            <div class="footer">
-                                <div id="response"></div>
+                                </div>
+                                <button 
+                                        class="save bg-purple-500 text-gray-50 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                        Sauvegarder l'ordre
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="lg:w-1/4 w-4/6">
-                <div class="flex flex-wrap  mb-4">
-                    <div class="md:w-full pr-4 pl-4">
-                        <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
-                            <div class="body">
-                                    {!! Form::open(['route' => 'sliderimage.store' , 'class'=>'avatar', 'enctype'=>'multipart/form-data' ,'data-parsley-validate' =>'','files'=>true ]) !!}
-                                    <input type="hidden" name="sitesliders_id" value="{{$slider->id}}">
-                                    <div class="slim" 
-                                        data-label="Ajouter" 
-                                        data-force-type="jpg"
-                                        data-size="{{$slider->width}},{{$slider->height}}" 
-                                        data-instant-edit="true"
-                                        data-button-cancel-label="Annuler" 
-                                        data-button-confirm-label="Confirmer" 
-                                        data-push="true"
-                                        data-save-initial-image="true" 
-                                        data-ratio="{{$slider->ratio}}">
-                                        <input type="file" name="slide[]" required />
+        </div>
+        <div class="lg:w-1/4 pr-4 pl-4 md:w-full">
+            <div class="flex flex-wrap  mb-4">
+                <div class="md:w-full pr-4 pl-4">
+                    <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
+                        <div class="body">
+                            {!! Form::open(['route' => 'sliderimage.store' , 'class'=>'avatar', 'enctype'=>'multipart/form-data' ,'data-parsley-validate' =>'','files'=>true ]) !!}
+                                <input type="hidden" name="sitesliders_id" value="{{$slider->id}}">
+                                <div class="slim" 
+                                    data-label="Ajouter" 
+                                    data-force-type="jpg"
+                                    data-size="{{$slider->width}},{{$slider->height}}" 
+                                    data-instant-edit="true"
+                                    data-button-cancel-label="Annuler" 
+                                    data-button-confirm-label="Confirmer" 
+                                    data-push="true"
+                                    data-save-initial-image="true" 
+                                    data-ratio="{{$slider->ratio}}">
+                                    <input type="file" name="slide[]" required />
                                 </div>
                                 <div class="flex flex-wrap  mb-4 form-float" style="margin-top:25px;">
                                     <div class="md:w-full pr-4 pl-4">
@@ -123,8 +119,7 @@
                                     class="w-full bg-blue-400 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">                                        
                                     Enregistrer
                                 </button>
-                                {!! Form::close() !!}
-                            </div>
+                            {!! Form::close() !!}
                         </div>
                     </div>
                 </div>
