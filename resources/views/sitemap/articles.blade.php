b5164f86acfb6975bac319081e904e3bd62387f4
minh-bao <mbnguyen@hotmail.fr>
2021-04-25 17:57:03 +0200

    fix url syntax

--- resources/views/sitemap/articles.blade.php
+++ resources/views/sitemap/articles.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($articles as $article)
         <url>
-            <loc>{{url('/' . {{ $article->slug }})}}/</loc>
+            <loc>{{url('/' . $article->slug)}}/</loc>
             <lastmod>{{ $article->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>daily</changefreq>
             <priority>0.9</priority>
