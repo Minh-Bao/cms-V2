5ef183ac38b719ac29cb6a97caa3794103b86939
minh-bao <mbnguyen@hotmail.fr>
2021-05-01 17:48:44 +0200

    add view for TableHeader component

---
+++ resources/views/components/table-header.blade.php
@@ -0,0 +1,14 @@
+<th wire:click="setOrderField('{{$name}}')"
+    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+    {{ $slot }}
+    @if($visible == 'ASC')
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11l5-5m0 0l5 5m-5-5v12" />
+        </svg>
+
+    @else
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+        </svg>
+    @endif
+</th>
