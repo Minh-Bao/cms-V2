37bb7e1ae40c4b541f9435a0e62bbebe763b8a43
minh-bao <mbnguyen@hotmail.fr>
2021-05-03 10:40:22 +0200

    fix css for search input

--- resources/views/components/table-header.blade.php
+++ resources/views/components/table-header.blade.php
@@ -1,5 +1,5 @@
 <th wire:click="setOrderField('{{$name}}')"
-    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
     {{ $slot }}
     @if($visible)
         @if($direction == 'ASC')
