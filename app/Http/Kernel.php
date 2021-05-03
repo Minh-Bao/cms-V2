daa2d1b8b45f7df6fa16b8aecef26f8a33b6ed52
minh-bao <mbnguyen@hotmail.fr>
2021-05-03 10:09:46 +0200

    register HttpsProtocolMiddleware in group midlleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\HttpsProtocolMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -37,7 +38,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HttpsProtocolMiddleware::class
+            // \App\Http\Middleware\HttpsProtocolMiddleware::class
+            
         ],
 
         'api' => [
@@ -63,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'https' => \App\Http\Middleware\HttpsProtocolMiddleware::class,
     ];
 }
