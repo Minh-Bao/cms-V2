33504d4c82efbfc585d466203b1c6f6923a6c5e3
minh-bao <mbnguyen@hotmail.fr>
2021-05-03 10:21:44 +0200

    correct all comment in english and comment home route because its a duplicate of admin.index route

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/sitemap.xml/articles', [SitemapController::class,'articles']);
 Route::post('/contact/send', [ContactController::class,'send'])->name('contact');
 
 Route::get('/', [ SiteController::class,'index'])->name('site.homepage')->middleware('https');
-Route::get('/home', [AdminController::class,'index'])->name('home');
+// Route::get('/home', [AdminController::class,'index'])->name('home');
 Route::get('/feed', 'App\Http\Controllers\FeedController@xml')->name('feed');
 Route::post('/send', [SiteController::class, 'form'])->name('site.send.form');
 
@@ -37,27 +37,27 @@ Route::post('/send', [SiteController::class, 'form'])->name('site.send.form');
 
 Route::prefix('admin')->group(function() {	
 
-	Route::get('/', [AdminController::class,'index'])->name('admin.index'); // Accueil
+	Route::get('/', [AdminController::class,'index'])->name('admin.index'); // Homepage
 
 	// Pages
 	Route::resource('/websitepage',WebsitepageController::class); // Pages
-	Route::get('/websitepage/{id}/delete',[WebsitepageController::class, 'destroy'])->name('websitepage.delete'); // Supprime une page
+	Route::get('/websitepage/{id}/delete',[WebsitepageController::class, 'destroy'])->name('websitepage.delete'); // delete page
 	Route::resource('/websitebloc',WebsiteblocController::class); // Blocs
-	Route::get('/websitebloc/{id}/clone',[WebsiteblocController::class, 'clone'])->name('websitebloc.clone'); // Clone d'un bloc
-	Route::post('/ajax/websitebloc/sort.json',[WebsiteblocController::class,'sort'])->name('bloc.sort'); // Tri des bloc
-	Route::get('/websitebloc/{id}/delete',[WebsiteblocController::class,'destroy'])->name('websiteblocs.destroy'); // Supprime un bloc
-	Route::put('/Websitepage/{id}', [WebsitepageController::class, 'setDate'])->name('websitepage.setDate'); // programme la publicarion
+	Route::get('/websitebloc/{id}/clone',[WebsiteblocController::class, 'clone'])->name('websitebloc.clone'); // Clone a bloc
+	Route::post('/ajax/websitebloc/sort.json',[WebsiteblocController::class,'sort'])->name('bloc.sort'); // Sort blocs
+	Route::get('/websitebloc/{id}/delete',[WebsiteblocController::class,'destroy'])->name('websiteblocs.destroy'); // delete a  bloc
+	Route::put('/Websitepage/{id}', [WebsitepageController::class, 'setDate'])->name('websitepage.setDate'); // Schedule a publication
 
 	// Sliders
 	Route::resource('/slider',SliderController::class); //Sliders
-	Route::get('/slider/{id}/delete',[SliderController::class,'destroy'])->name('slider.delete'); // Supprime un slider
-	Route::resource('/sliderimage',SliderImageController::class); //Images des sliders
-	Route::post('/ajax/sliderimage/sort.json',[SliderImageController::class,'sort'])->name('sliderimage.sort'); // Tri des slides
-	Route::get('/sliderimage/{id}/delete',[SliderImageController::class, 'delete'])->name('sliderimage.delete'); // Supprime un slide
+	Route::get('/slider/{id}/delete',[SliderController::class,'destroy'])->name('slider.delete'); // Delete a  slider
+	Route::resource('/sliderimage',SliderImageController::class); //slider's imagess
+	Route::post('/ajax/sliderimage/sort.json',[SliderImageController::class,'sort'])->name('sliderimage.sort'); // sort slides
+	Route::get('/sliderimage/{id}/delete',[SliderImageController::class, 'delete'])->name('sliderimage.delete'); // Delete a  slide
 
 	Route::post('/config/{id}/update',[SitebuilderController::class,'update'])->name('sitebuilder.config.update'); 
 	Route::get('/config/{id}/change',[SitebuilderController::class,'change'])->name('sitebuilder.image.change'); 
-	Route::get('/sitebuilder',[SitebuilderController::class,'element'])->name('sitebuilder'); // Sitebuilder
+	Route::get('/sitebuilder',[SitebuilderController::class,'element'])->name('sitebuilder'); // Sitebuilder controller
 
 
 });
