a2a32beff4df44b40be1e41d0f715bde92c22ab8
minh-bao <mbnguyen@hotmail.fr>
2021-05-04 20:13:00 +0200

    reset page when making a search to search in all page

--- app/Http/Livewire/PagesTable.php
+++ app/Http/Livewire/PagesTable.php
@@ -14,7 +14,6 @@ class PagesTable extends Component
     public  $search = "";
     public  $orderField = "created_at";
     public  $orderDirection = 'ASC';
-    // public  $editId = 0;
 
     protected $queryString = [
         'search' => ['except' => ''],
@@ -23,11 +22,11 @@ class PagesTable extends Component
 
     ];
 
-    // public function startEdit(int $id) 
-    // {
-    //     $this->editId = $id;
-    // }
-
+    public function updating($name, $value) {
+        if ($name == 'search') {
+            $this->resetPage();
+        }
+    }
 
     public function setOrderField(string $name) 
     {
