6a268418046d56532de8782505a477bfc2538e3a
minh-bao <mbnguyen@hotmail.fr>
2021-05-19 22:28:54 +0200

    add condition for when there is only one page made in tabase and to display

--- resources/views/site/themes/cms/blocs/group-article-full.blade.php
+++ resources/views/site/themes/cms/blocs/group-article-full.blade.php
@@ -55,31 +55,33 @@
     </aside>
 </section>
 
-@if ($next == "nonext")
-    <div id="pagination"  style="width:63%;"
-        class=" pl-4 pink_nc mx-12 pr-12 border border-t-2 border-pink-450" >
-        <div style="display:flex; justify-content: space-evenly;margin-bottom:5%;padding-top:2%" >
-            <a style="color:rgb(168, 19, 76) !important" href="{{url('/').'/page/'.$prev}}">Article précédent > </a>
-        </div>
-    </div>
-@else
-    @if ($prev == "noprev")
-        <div id="pagination" style="width:63%;"
-             class=" pl-4 pink_nc mx-12 pr-12 border-t-2 border-pink-450" >
+@isset($oneOnly)
+    @if ($next == "nonext")
+        <div id="pagination"  style="width:63%;"
+            class=" pl-4 pink_nc mx-12 pr-12 border border-t-2 border-pink-450" >
             <div style="display:flex; justify-content: space-evenly;margin-bottom:5%;padding-top:2%" >
-                <a style="color:rgb(168, 19, 76) !important" href="{{url('/') . '/page/'.$next}}">< Article suivant </a>
+                <a style="color:rgb(168, 19, 76) !important" href="{{url('/').'/page/'.$prev}}">Article précédent > </a>
             </div>
         </div>
     @else
-        <div id="pagination" style="width:63%;"
-            class= "pl-4 pink_nc m-12 pr-12 border-t-2 border-pink-450" >
-            <div style="display:flex; justify-content: space-evenly;margin-bottom:5%;padding-top:2%" >
-                <a style="color:rgb(168, 19, 76) !important" href="{{url('/') .'/page/'.$next}}">< Article suivant </a>
-                <a style="color:rgb(168, 19, 76) !important" href="{{url('/').'/page/'.$prev}}"> Article précédent ></a>
+        @if ($prev == "noprev")
+            <div id="pagination" style="width:63%;"
+                class=" pl-4 pink_nc mx-12 pr-12 border-t-2 border-pink-450" >
+                <div style="display:flex; justify-content: space-evenly;margin-bottom:5%;padding-top:2%" >
+                    <a style="color:rgb(168, 19, 76) !important" href="{{url('/') . '/page/'.$next}}">< Article suivant </a>
+                </div>
             </div>
-        </div>    
+        @else
+            <div id="pagination" style="width:63%;"
+                class= "pl-4 pink_nc m-12 pr-12 border-t-2 border-pink-450" >
+                <div style="display:flex; justify-content: space-evenly;margin-bottom:5%;padding-top:2%" >
+                    <a style="color:rgb(168, 19, 76) !important" href="{{url('/') .'/page/'.$next}}">< Article suivant </a>
+                    <a style="color:rgb(168, 19, 76) !important" href="{{url('/').'/page/'.$prev}}"> Article précédent ></a>
+                </div>
+            </div>    
+        @endif
     @endif
-@endif
+@endisset
 
 
 
