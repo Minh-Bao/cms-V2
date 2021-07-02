1f85cc422ba6d83ac0309c51893e8eb523be331e
minh-bao <mbnguyen@hotmail.fr>
2021-07-02 19:53:12 +0200

    set slider only if the variable is set

--- resources/views/admin/site/slider/index.blade.php
+++ resources/views/admin/site/slider/index.blade.php
@@ -60,42 +60,43 @@
                 </div>
 
                 <div class="flex flex-wrap mt-4">
-                    @foreach ($slide as $chunk) 
-                    <div class="bg-white w-1/2">
-                        <div class="ml-8">
-                            <div class="flex flex-wrap" >
-                                <div class="w-1/2 ">
-                                    <h2 class="  text-gray-900 text-xl">
-                                        Slides : {{$chunk[2]['title']}}
-                                    </h2>
-                                    <p class="mt-3 max-w-3xl text-lg text-gray-500">
-                                        Deplacer les images pour modifier l'ordre des slides...
-                                    </p>
-                                    <div class="mt-8 sm:flex">
-                                        <div class="mt-3 sm:mt-0 ">
-                                            <button
-                                                class=" save order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-50 bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 ">
-                                                Enregistrer
-                                            </button>
+                    @isset($slide)
+                        @foreach ($slide as $chunk) 
+                        <div class="bg-white w-1/2">
+                            <div class="ml-8">
+                                <div class="flex flex-wrap" >
+                                    <div class="w-1/2 ">
+                                        <h2 class="  text-gray-900 text-xl">
+                                            Slides : {{$chunk[2]['title']}}
+                                        </h2>
+                                        <p class="mt-3 max-w-3xl text-lg text-gray-500">
+                                            Deplacer les images pour modifier l'ordre des slides...
+                                        </p>
+                                        <div class="mt-8 sm:flex">
+                                            <div class="mt-3 sm:mt-0 ">
+                                                <button
+                                                    class=" save order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-50 bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 ">
+                                                    Enregistrer
+                                                </button>
+                                            </div>
+                                        </div> 
+                                    </div>
+                                    <div class="w-1/2 ">
+                                        <div class="mt-8 grid grid-cols-3 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-3 sortable">
+                                            @for ($i =0; $i<count($chunk) ; $i++)
+                                                <div id="item-{{$chunk[$i]['id']}}" class="col-span-1 flex justify-center bg-gray-50 bg-white border-1 border-gray-300 droppable">
+                                                    <img class="w-100" src="{{asset($chunk[$i]['picture'])}}"
+                                                        alt="Workcation" id="{{$chunk[$i]['id']}}" name="noname">
+                                                </div>                              
+                                            @endfor
                                         </div>
-                                    </div> 
+                                        </div>
                                 </div>
-                                <div class="w-1/2 ">
-                                    <div class="mt-8 grid grid-cols-3 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-3 sortable">
-                                        @for ($i =0; $i<count($chunk) ; $i++)
-                                            <div id="item-{{$chunk[$i]['id']}}" class="col-span-1 flex justify-center bg-gray-50 bg-white border-1 border-gray-300 droppable">
-                                                <img class="w-100" src="{{asset($chunk[$i]['picture'])}}"
-                                                    alt="Workcation" id="{{$chunk[$i]['id']}}" name="noname">
-                                            </div>                              
-                                        @endfor
-                                    </div>
-                                    </div>
                             </div>
-                        </div>
-                    </div>     
-                    @endforeach
-                    </div>
-                
+                        </div>     
+                        @endforeach                        
+                    @endisset
+                </div>              
 
 
                 <div class=" mt-8 ">
@@ -238,35 +239,23 @@
 
 $(document).ready( function () {
 
+@if($sliders->count() == 0 )
 
+    toastr.warning('Pour ajouter votre premier Slider, cliquer sur le bouton AJOUTER');
 
-@if($sliders->count()==0)
-
-toastr.warning('Pour ajouter votre premier Slider, cliquer sur le bouton AJOUTER');
-
-$('#btn-slider-create').animateCss("bounce");
-
-
+    $('#btn-slider-create').animateCss("bounce");
 
 @endif
 
   $('#sliders-tab').dataTable( {
-
     "bStateSave": true,
-
     "oLanguage": { "sUrl": "{{ url('/fr.txt') }}" },
-
     dom: 'Bfrtip'
-
-
-
   });
 
 });
 
 </script>
-
-
 
 @endsection
 
