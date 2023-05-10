a997220caa26bf16c8e26697ec4d91108e167e10
minh-bao <mbnguyen@hotmail.fr>
2023-05-10 21:44:20 +0200

    refactor(resource): add condition if variable don't exist

--- resources/views/components/table-header.blade.php
+++ resources/views/components/table-header.blade.php
@@ -1,7 +1,7 @@
 <th wire:click="setOrderField('{{$name}}')"
     class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
     {{ $slot }}
-    @if($visible)
+    @if($visible ?? '')
         @if($direction == 'ASC')
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
